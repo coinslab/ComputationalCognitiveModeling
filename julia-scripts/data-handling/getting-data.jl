f5b65efca6bb1b9d6b5d38174e2d74fb44c54a13
Athul Sudheesh <athuls0708@gmail.com>
2021-01-07 16:00:43 -0600

    getting-data.jl added

---
+++ julia-scripts/data-handling/getting-data.jl
@@ -0,0 +1,18 @@
+# To download data from a url, use the below code 
+
+url = "https://gist.githubusercontent.com/curran/a08a1080b88344b0c8a7/raw/0e7a9b0a5d22642a06d3d5b9bcbad9890c8ee534/iris.csv"
+dataname = "iris.csv" # Here you can give any name you wish instead of iris 
+download(url, dataname)
+# =================================================
+
+
+# To load CSV Files from your working directory 
+using CSV # This is a pacakge we use for loading CSV Files.
+using DataFrames
+data = CSV.read("iris.csv", DataFrame) # This will load the dataset and convert it into a DataFrame
+# =================================================
+
+# To load it from Packages like RDatasets
+using RDatasets 
+data = dataset("datasets", "iris")
+# =================================================
