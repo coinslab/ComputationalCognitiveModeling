158f4be77c743eaaa6b682de898066bf70c28579
Athul Sudheesh <athuls0708@gmail.com>
2021-01-26 11:52:12 -0600

    Update eulertest.jl

--- julia-scripts/scripts-theory/eulertest.jl
+++ julia-scripts/scripts-theory/eulertest.jl
@@ -1,23 +1,31 @@
-using Plots
-function euler(x₀, δ_t, nrsteps)
+using  Plots
+function euler(x₀, δ_t,nrsteps)
     x = x₀
     for i in 1:nrsteps
-        dxdt = (x - 3) * (x + 6) * x
-        x = x + δ_t * dxdt
+        dxdt = (x-3)*(x+6)*x
+        x = x+ δ_t * dxdt
     end
     return x
 end
 
-function eulerarray(x0, deltat, nrsteps)
+# Each function should only have one task to do 
+function eulerarray(x0, deltat,nrsteps)
     x = Float64[] # Initialize an empty array
     append!(x, x0)
     for i in 1:nrsteps
         dxdt = (x[i] - 3) * (x[i] + 6) * x[i]
         temp = x[i] + deltat * dxdt
-        append!(x, temp)
+        append!(x,temp)
     end
-    plot(x, 
-        xlabel="time", 
-         ylabel="x array",
-         title="Euler Example", legend=false)
+    return x 
+end
+"""
+Demo function for Euler method
+    - returns a plot 
+"""
+function eulerarraydemo(x0,deltat,nrsteps)
+    x = eulerarray(x0,deltat,nrsteps)
+    plot(x, xlabel = "Time t", ylabel = "x", 
+            title = "Euler Example", 
+            legend=false)
 end
