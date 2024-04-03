91e4fce6c355466fe248d05293048f3310640ef3
Ritesh Kumar Malaiya <ritesh.malaiya@gmail.com>
2024-04-03 11:34:42 -0500

    Update MNISTmlpKeras.py
    fixed a bug

--- python-scripts/MNISTmlpKeras.py
+++ python-scripts/MNISTmlpKeras.py
@@ -8,46 +8,47 @@ import pandas as pd
 
 # This was the original code in the book. 
 # We will not be using this line of code in our assignment
-#(xtrain,ytrain), (xtest,ytest) = datasets.mnist.loaddata()
+#(xtrain,ytrain), (xtest,ytest) = datasets.mnist.load_data()
 
 # Start: New code added for assignment
 # This parameter specifies how many columns, counting from the left, are output columns.
+
 K = 1
 
 train = pd.read_excel("data/trainingdata.xlsx").to_numpy()
 xtrain = train[:, K:]
-ytrain = train[:, :K]
+ytrain = train[:, :K].flatten()
 
 test = pd.read_excel("data/testingdata.xlsx").to_numpy()
 xtest = test[:, K:] 
-ytest = test[:, :K]
+ytest = test[:, :K].flatten()
 
 input_nodes = xtrain.shape[1]
 
 # End: New code added for assignment
 
 # Made minor updates to the original code from the book
-xtrain=xtrain/255
-xtest=xtest/255
+#xtrain=xtrain/255
+#xtest=xtest/255
 
 # This was the original code in the book. 
 # We will not be using this line of code in our assignment
-#ytrain=utils.tocategorical(ytrain,10)
-#ytest=utils.tocategorical(ytest,10)
+#ytrain=utils.to_categorical(ytrain,10)
+#ytest=utils.to_categorical(ytest,10)
 
 inputs=layers.Input(shape=(input_nodes,))
 x=layers.Dense(128,activation='relu')(inputs)
-x=layers.Dense(128,activation='relu')(x)
-x=layers.Dense(128,activation='relu')(x)
-x=layers.Dense(128,activation='relu')(x)
-x=layers.Dense(128,activation='relu')(x)
+#x=layers.Dense(128,activation='relu')(x)
+#x=layers.Dense(128,activation='relu')(x)
+#x=layers.Dense(128,activation='relu')(x)
+#x=layers.Dense(128,activation='relu')(x)
 
 # Made minor updates to the original code from the book
 outputs=layers.Dense(K,activation='softmax')(x)
 
 model=models.Model(inputs=inputs,outputs=outputs)
 
-model.compile(loss='categorical_crossentropy',
+model.compile(loss='binary_crossentropy',
 optimizer='Nadam',metrics=['accuracy'])
 
 history = model.fit(xtrain,ytrain,
